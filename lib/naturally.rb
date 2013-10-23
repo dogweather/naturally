100d8786e991a061e16ebf15a64d634f4f0cbf22
KD <kd.engineer@yahoo.co.in>
2013-10-23 16:24:53 +0530

    added sort case when numbers having digit at the end and rspecs

--- lib/naturally.rb
+++ lib/naturally.rb
@@ -34,6 +34,8 @@ module Naturally
         return @val.to_i <=> other.val.to_i
       elsif numbers_with_letters? || other.numbers_with_letters?
         return simple_normalize(@val) <=> simple_normalize(other.val)
+      elsif letters_with_numbers? || other.letters_with_numbers?
+        return reverse_simple_normalize(@val) <=> reverse_simple_normalize(other.val)
       else
         return @val <=> other.val
       end
@@ -46,6 +48,10 @@ module Naturally
     def numbers_with_letters?
       val =~ /^\d+[a-zA-Z]+$/
     end
+
+    def letters_with_numbers?
+      val =~ /^[a-zA-Z]+\d+$/
+    end
     
     def simple_normalize(n)
       if n =~ /^(\d+)([a-zA-Z]+)$/
@@ -53,7 +59,15 @@ module Naturally
       else 
         return [n.to_i]
       end
-    end    
+    end
+
+    def reverse_simple_normalize(_value)
+      if _value =~ /^([a-zA-Z]+)(\d+)$/
+        return [$1, $2.to_i]
+      else
+        return [_value.to_s]
+      end
+    end
   end
   
 end
