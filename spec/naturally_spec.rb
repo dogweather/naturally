38aa129304a09b0a0f343ca8a8aaff2a3261d55b
Robb Shecter <robb@public.law>
2018-06-03 02:42:19 -0700

    refactor: coding style

--- spec/naturally_spec.rb
+++ spec/naturally_spec.rb
@@ -2,12 +2,14 @@
 require 'naturally'
 
 describe Naturally do
+  # Just a helper for these tests
   def it_sorts(opts = {})
     this = opts[:this]
     to_this = opts[:to_this]
     actual = Naturally.sort(this)
     expect(actual).to eq(to_this)
   end
+
 
   describe '#sort' do
     it 'supports a nicer by: syntax' do
@@ -36,56 +38,56 @@ describe Naturally do
 
     it 'sorts an array of strings nicely as if they were legal numbers' do
       it_sorts(
-        this: %w(676 676.1 676.11 676.12 676.2 676.3 676.9 676.10),
+        this:    %w(676 676.1 676.11 676.12 676.2 676.3 676.9 676.10),
         to_this: %w(676 676.1 676.2 676.3 676.9 676.10 676.11 676.12)
       )
     end
 
     it 'sorts a more complex list of strings' do
       it_sorts(
-        this: %w(350 351 352 352.1 352.5 353.1 354 354.3 354.4 354.45 354.5),
+        this:    %w(350 351 352 352.1 352.5 353.1 354 354.3 354.4 354.45 354.5),
         to_this: %w(350 351 352 352.1 352.5 353.1 354 354.3 354.4 354.5 354.45)
       )
     end
 
     it 'sorts when numbers have letters in them' do
       it_sorts(
-        this: %w(335 335.1 336a 336 337 337a 337.1 337.15 337.2),
+        this:    %w(335 335.1 336a 336 337 337a 337.1 337.15 337.2),
         to_this: %w(335 335.1 336 336a 337 337.1 337.2 337.15 337a)
       )
     end
 
     it 'sorts when numbers have unicode letters in them' do
       it_sorts(
-        this: %w(335 335.1 336a 336 337 337я 337.1 337.15 337.2),
+        this:    %w(335 335.1 336a 336 337 337я 337.1 337.15 337.2),
         to_this: %w(335 335.1 336 336a 337 337.1 337.2 337.15 337я)
       )
     end
 
     it 'sorts when letters have numbers in them' do
       it_sorts(
-        this: %w(PC1, PC3, PC5, PC7, PC9, PC10, PC11, PC12, PC13, PC14, PROF2, PBLI, SBP1, SBP3),
+        this:    %w(PC1, PC3, PC5, PC7, PC9, PC10, PC11, PC12, PC13, PC14, PROF2, PBLI, SBP1, SBP3),
         to_this: %w(PBLI, PC1, PC3, PC5, PC7, PC9, PC10, PC11, PC12, PC13, PC14, PROF2, SBP1, SBP3)
       )
     end
 
     it 'sorts when letters have numbers and unicode characters in them' do
       it_sorts(
-        this: %w(АБ4, АБ2, АБ10, АБ12, АБ1, АБ3, АД8, АД5, АЩФ12, АЩФ8, ЫВА1),
+        this:    %w(АБ4, АБ2, АБ10, АБ12, АБ1, АБ3, АД8, АД5, АЩФ12, АЩФ8, ЫВА1),
         to_this: %w(АБ1, АБ2, АБ3, АБ4, АБ10, АБ12, АД5, АД8, АЩФ8, АЩФ12, ЫВА1)
       )
     end
 
     it 'sorts double digits with letters correctly' do
       it_sorts(
-        this: %w(12a 12b 12c 13a 13b 2 3 4 5 10 11 12),
+        this:    %w(12a 12b 12c 13a 13b 2 3 4 5 10 11 12),
         to_this: %w(2 3 4 5 10 11 12 12a 12b 12c 13a 13b)
       )
     end
 
     it 'sorts double digits with unicode letters correctly' do
       it_sorts(
-        this: %w(12а 12б 12в 13а 13б 2 3 4 5 10 11 12),
+        this:    %w(12а 12б 12в 13а 13б 2 3 4 5 10 11 12),
         to_this: %w(2 3 4 5 10 11 12 12а 12б 12в 13а 13б)
       )
     end
@@ -99,28 +101,28 @@ describe Naturally do
 
     it 'sorts letters with digits correctly' do
       it_sorts(
-        this: %w(1 a 2 b 3 c),
+        this:    %w(1 a 2 b 3 c),
         to_this: %w(1 2 3 a b c)
       )
     end
 
     it 'sorts complex numbers with digits correctly' do
       it_sorts(
-        this: %w(1 a 2 b 3 c 1.1 a.1 1.2 a.2 1.3 a.3 b.1 ),
+        this:    %w(1 a 2 b 3 c 1.1 a.1 1.2 a.2 1.3 a.3 b.1 ),
         to_this: %w(1 1.1 1.2 1.3 2 3 a a.1 a.2 a.3 b b.1 c)
       )
     end
 
     it 'sorts complex mixes of numbers and digits correctly' do
       it_sorts(
-        this: %w( 1.a.1 1.1 ),
+        this:    %w( 1.a.1 1.1 ),
         to_this: %w( 1.1 1.a.1 )
       )
     end
 
     it 'sorts complex mixes of numbers and digits correctly' do
       it_sorts(
-        this: %w( 1a1 1aa aaa ),
+        this:    %w( 1a1 1aa aaa ),
         to_this: %w( 1aa 1a1 aaa )
       )
     end
